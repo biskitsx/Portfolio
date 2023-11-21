9df4d2ea2e3e65e2f685dc043228608d795b853d
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-21 14:39:58 +0700

    add prop.github

--- src/components/SubProject.tsx
+++ src/components/SubProject.tsx
@@ -5,11 +5,13 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import Image from 'next/image'
 
 interface ProjectProps {
-    url: string
+    url?: string
+    github: string
     image: string
     title: string
     desc: string
     tools: string[]
+
 }
 
 interface SubProjectProps {
@@ -21,20 +23,27 @@ function SubProject({ props }: SubProjectProps) {
             return
         }
 
-        window.location.href = props.url;
+        // window.location.href = props.url;
     };
 
     return (
-        <div className="card  shadow-2xl bg-base-100 card-compact hover:scale-105 transition-all cursor-pointer rounded-md" onClick={handleLinkClick}>
+        <div className="card  shadow-2xl bg-base-100 card-compact hover:scale-105 transition-all rounded-md" onClick={handleLinkClick}>
             <figure className=''>
                 <Image src={props.image} alt="go" className=' h-56 w-full object-cover' width={1000} height={1000} />
             </figure>
             <div className="card-body">
                 <h2 className="card-title ">
-                    {props.title}
-                    <a className="badge bg-purple-700 text-white flex gap-1 hover:scale-105" href={props.url}>
-                        <FontAwesomeIcon icon={faGithub} /> Github
+                    <p>
+                        {props.title}
+                    </p>
+                    <a className="badge badge-secondary  text-white  hover:scale-125 cursor-pointer" href={props.github}>
+                        <FontAwesomeIcon icon={faGithub} />
                     </a>
+                    {props.url &&
+                        <a className="badge badge-accent text-white flex gap-1 hover:scale-125 cursor-pointer" href={props.url}>
+                            <FontAwesomeIcon icon={faLink} />
+                        </a>
+                    }
                 </h2>
                 <p>{props.desc}</p>
                 <div className="card-actions justify-end">
