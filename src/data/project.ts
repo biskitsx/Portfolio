37083e4232802990a760067e7669acd5ddbd5e21
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-11-21 14:40:06 +0700

    add kittoshop project

--- src/data/project.ts
+++ src/data/project.ts
@@ -1,94 +1,104 @@
 export const projectDetail = [
     {
+        title: "kittoshop",
+        image: "https://img5.pic.in.th/file/secure-sv1/-2566-11-08--20.11.37.png",
+        desc: 'An E-commerce web application "kittoshop" built by nextjs, go',
+        tools: ["Go", "NextJS", "TailwindCSS", "TypeScript"],
+        url: "https://kittoshop.vercel.app",
+        github: "https://github.com/biskitsx/kittoshop"
+    },
+    {
         title: "kittotype",
         image: "/kittotype.png",
         desc: "A minimalistic typing test web application that allows users to see live errors, WPM, and accuracy displays",
         tools: ["NextJS", "TailwindCSS", "TypeScript"],
-        url: "https://kittotype.vercel.app"
+        url: "https://kittotype.vercel.app",
+        github: "https://github.com/biskitsx/kittotype"
     },
     {
         title: "Mood & Sound",
         image: "/mood.png",
         desc: "This Web application will display various sounds for users to choose from based on their mood.",
         tools: ["NextJS", "TailwindCSS", "TypeScript"],
-        url: "https://mood-sound.vercel.app"
+        url: "https://mood-sound.vercel.app",
+        github: "https://github.com/biskitsx/mood-sound"
     },
     {
         title: "ZideQuest",
         image: "/zq.jpg",
         desc: "A Geographic Information Systems application that shows events happening around campus using Google Maps API 🌍",
         tools: ["React Native", "NodeJS", "MongoDB", "Express"],
-        url: "https://github.com/ZideQuest/ZideQuest-Backend"
+        github: "https://github.com/ZideQuest/ZideQuest-Backend"
     },
     {
         title: "Grocery",
         image: "/grocery.png",
         desc: "This repository contains the source code for a grocery web application that is built using Golang and React. The application allows users to browse a catalog of products, add items to their cart 🥦",
         tools: ["Go", "Fiber", "MySQL", "ReactJS"],
-        url: "https://github.com/biskitsx/Grocery"
+        github: "https://github.com/biskitsx/Grocery"
     },
     {
         title: "GoApi WebApp",
         image: "/goo.webp",
         desc: "This GitHub repository showcases a simple web API implementation using the Fiber framework in Go.",
         tools: ["Go", "Fiber", "Gorm", "PostgreSQL"],
-        url: "https://github.com/biskitsx/GoAPI-WebApp"
+        github: "https://github.com/biskitsx/GoAPI-WebApp"
     },
     {
         title: "Real time web chat",
         image: "/chat.png",
         desc: "A Real-time chat application using React with TypeScript on the frontend and Express with TypeScript and Socket.IO on the backend.💬",
         tools: ["ReactJS", "MongoDB", "Express"],
-        url: "https://github.com/biskitsx/RealTime-Chat-App"
+        github: "https://github.com/biskitsx/RealTime-Chat-App"
     },
     {
         title: "Social App",
         image: "/social.png",
         desc: "This repository showcases the development of a simple social application using the MERN stack",
         tools: ["NodeJS", "MongoDB", "Express"],
-        url: "https://github.com/biskitsx/social-appx"
+        github: "https://github.com/biskitsx/social-appx"
     },
     {
         title: "Leave Work",
         image: "/home.png",
         desc: "Web app for tracking user leave data, including leave date, type, notes, and entry date. Authenticate users to ensure data security.",
         tools: ["Golang", "Fiber", "PostgreSQL", "NextJS"],
-        url: "https://github.com/biskitsx/LeaveWork-Management-App"
+        github: "https://github.com/biskitsx/LeaveWork-Management-App"
     },
     // {
     //     title: "Ecommerce App",
     //     image: "/ecommerce.png",
     //     desc: "This GitHub repository showcases a full-fledged e-commerce application developed using the MERN (MongoDB, Express.js, React, Node.js) stack. 🛒",
     //     tools: ["NodeJS", "MongoDB", "Express"],
-    //     url: "https://github.com/biskitsx/Ecommerce"
+    //     github: "https://github.com/biskitsx/Ecommerce"
     // },
     {
         title: "PromptPay QRCode Generator",
         image: "/aftergen.png",
         desc: "Web application and JavaScript library to generate QR Code payload for PromptPay. 🌈",
         tools: ["NodeJS", "TypeScript", "Express", "QRCode"],
-        url: "https://github.com/biskitsx/PromptPay-QRCode-Generator"
+        github: "https://github.com/biskitsx/PromptPay-QRCode-Generator"
     },
     {
         title: "aws deploy golang",
         image: "/aws.avif",
         desc: "Learn to use Docker with multiple services, such as API (Go) and database, and deploy on Amazon Web Services (EC2). 🔥",
         tools: ["docker", "golang", "deploy", "aws-ec2"],
-        url: "https://github.com/biskitsx/aws-deploy-golang"
+        github: "https://github.com/biskitsx/aws-deploy-golang"
     },
     // {
     //     title: "Redis NodeJS",
     //     image: "/redis.png",
     //     desc: "A simple and straightforward implementation of a caching system in TypeScript using Redis.",
     //     tools: ["Redis", "NodeJS", "TypeScript", "Express"],
-    //     url: "https://github.com/biskitsx/Ecommerce"
+    //     github: "https://github.com/biskitsx/Ecommerce"
     // },
     // {
     //     title: "NestJS",
     //     image: "/nest.jpeg",
     //     desc: "The integration of Prisma and Nest.js to build a powerful API. Prisma is an ORM (Object-Relational Mapping) tool that simplifies database access and management. 🦁",
     //     tools: ["Redis", "NodeJS", "TypeScript", "Express"],
-    //     url: "https://github.com/biskitsx/nest-js"
+    //     github: "https://github.com/biskitsx/nest-js"
     // },
 
 ]
